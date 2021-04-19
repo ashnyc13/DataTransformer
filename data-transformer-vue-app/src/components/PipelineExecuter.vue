a6da39cfae346c38735d09fa3835059f24d913cb
Ash Arya <ashutosharyan@gmail.com>
2021-04-19 18:42:05 -0400

    - Added bootstrap CSS - Added PipelineExecuter component

---
+++ data-transformer-vue-app/src/components/PipelineExecuter.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <b-form-textarea
+      id="inputTextArea"
+      v-model="input"
+      placeholder="Type/paste input data here"
+      rows="3"
+      max-rows="6"
+    ></b-form-textarea>
+
+    <p>
+      <b-button @click="executeSelectedPipeline">Execute Pipeline</b-button>
+      |
+      <b-button @click="copyOutputToInput">Copy to input</b-button>
+    </p>
+
+    <b-form-textarea
+      id="outputTextArea"
+      v-model="output"
+      placeholder="Output data will be available here after processing"
+      rows="3"
+      max-rows="6"
+      :readonly="true"
+    ></b-form-textarea>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      input: "",
+      output: "",
+    };
+  },
+  methods: {
+    executeSelectedPipeline() {
+      // TODO: Execute selected pipeline
+      this.output = "Output from pipeline";
+    },
+    copyOutputToInput() {
+      this.input = this.output;
+    },
+  },
+};
+</script>
--- data-transformer-vue-app/src/views/Home.vue
+++ data-transformer-vue-app/src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home-view-container">
+    <h1>Data Transformer</h1>
+
+    <b-form-select
+      v-model="selectedPipeline"
+      :options="allPipelines"
+    ></b-form-select>
+    <p></p>
+    <PipelineExecuter
+      :selectedPipeline="selectedPipeline"
+      v-if="selectedPipeline"
+    />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import PipelineExecuter from "@/components/PipelineExecuter";
 
 export default {
-  name: 'Home',
+  name: "Home",
+  data() {
+    return {
+      selectedPipeline: null,
+      allPipelines: [
+        { value: null, text: "Please select a pipeline" },
+        { value: "Ascii + Base64", text: "Ascii + Base64" },
+      ],
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    PipelineExecuter,
+  },
+};
 </script>
